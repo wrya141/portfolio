3f03b943cadd0c69dfcd814f37fd4db15c9e79ca
Wrya Baxtyar <wrya141@gmail.com>
2025-06-23 18:07:01 +0300

    change wrya141 to wrya

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -32,7 +32,7 @@ export default function Home() {
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
         <div className="absolute flex flex-col items-center sm:top-[60%] top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">
-            Wrya141
+            Wrya
           </h1>
           <p className="font-light text-foreground text-ls m-8">
             Meet the guy behind this projects
